e7b31f39802253a27749fddaf707ff9c00a31bee
Erick Arroyo <incspnsoft@gmail.com>
2020-12-12 19:28:17 +0000

    day 8 part 2

--- day8.hs
+++ day8.hs
@@ -1,4 +1,7 @@
 import System.IO(readFile)
+import Debug.Trace ( trace )
+
+debug = flip trace
 
 format :: [String] -> [(String, Int)]
 format str = let 
@@ -30,9 +33,37 @@ run dt (indx, acc) indexes = let
         else
             run dt (exec current (indx, acc)) (indexes ++ [indx])
 
+invert :: String -> String
+invert s = case s of
+    "jmp" -> "nop"
+    "nop" -> "jmp"
+    _ -> s
+
+trd :: (a, b, c) -> c
+trd (_, _, t) = t
+
+
+
+runp2 :: [(String, Int)] -> (Int, Int) -> [Int] -> ((Int, Int), Int) -> [Int] -> Bool -> Int
+runp2 dt (indx, acc) indexes lastExecution chgIndxs changed
+    | indx `elem` indexes && changed = runp2 dt (0,0) [] ((0,0),0) (chgIndxs ++ [snd lastExec]) False
+    | indx `elem` indexes = runp2 dt (fst lastExec) (filter (/= snd lastExec) indexes) lastExec chgIndxs True -- `debug` ("inv exec " ++ show (invert (fst current), snd current))
+    | indx == length dt = acc
+    | otherwise = runp2 dt (exec current (indx, acc)) (indexes ++ [indx]) lastExec chgIndxs changed
+    where 
+        current = dt!!indx
+        lastExec = if fst current `elem` ["jmp", "nop"] && not (indx `elem` chgIndxs)
+            then (exec (invert (fst current), snd current) (indx, acc), indx) -- `debug` show (invert (fst current), snd current, current, fst current)
+            else lastExecution
+
+
 main :: IO()
 main = do
-    file <- readFile "./day8_input.txt"
+    file <- readFile "./inp_day8.txt"
     let f = format . lines $ file 
     
-    print $ run f (0,0) []
+    -- part 1 
+    -- print $ run f (0,0) []
+    
+    -- part 2 
+    print $ runp2 f (0,0) [] ((0,0),0) [] False
