5769b7b2bfe9692694f66d43cd870290f43639ef
Erick Arroyo <incspnsoft@gmail.com>
2020-12-26 22:57:09 -0800

    day 17 part 2

--- day17.py
+++ day17.py
@@ -5,21 +5,29 @@ from itertools import product
 with open("inputs/inp_day17.txt", 'r') as file:
     dt = file.read().split("\n")
 
+"""
+dt = [
+        '.#.',
+        '..#',
+        '###']
+"""
+
 ps = tuple(
-    (x, y, 0)
+    (x, y, 0, 0)
     for y, l in enumerate(dt)
     for x, c in enumerate(l) if c == "#")
 
-trans = list(product((-1, 0, 1), repeat=3))
-trans.remove((0,)*3)
+trans = list(product((-1, 0, 1), repeat=4))
+trans.remove((0,)*4)
 
 
 def expand(p):
     for i in trans:
-        yield (p[0] + i[0], p[1] + i[1], p[2] + i[2])
+        yield (p[0] + i[0], p[1] + i[1], p[2] + i[2], p[3] + i[3])
 
 
 for _ in range(6):
+    print('----> ', 6-_)
     new = set()
     for ac in ps:
 
